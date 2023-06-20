d03c88e55704fe41aea10d92d3548f5b0e04a737
Malzagic <przemek2142@gmail.com>
2023-06-20 09:44:07 +0200

    start with portfolio pages

--- client/src/components/pages/Portfolio.jsx
+++ client/src/components/pages/Portfolio.jsx
@@ -8,21 +8,22 @@ const Portfolio = () => {
   const repos = ['portfolio', 'Feedback-UI', 'Github-Finder', 'ReactWeather-app', 'Track_calories', 'calculate-car-excise'];
 
   const [repositories, setRepositories] = useState(repos[0]);
+  const [active, setActive] = useState('portfolio');
+  const [data, setData] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const url = `http://localhost:8000/portfolio/`;
+      const response = await axios.get(url + repositories);
+      const jsonData = await response.data;
 
+      setData(jsonData[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const url = `http://localhost:8000/portfolio/`;
-        const response = await axios.get(url + repositories);
-        const jsonData = await response.data;
-  
-        console.log(jsonData[0])
-      } catch (error) {
-        console.error(error);
-      }
-    };
 
     fetchData()
   }, [repositories]);
@@ -30,6 +31,7 @@ const Portfolio = () => {
 
   const handleClick = (e) => {
     const repository = e.target.getAttribute('data-value')
+    setActive(repositories);
     setRepositories(repository);
   }
 
@@ -37,30 +39,42 @@ const Portfolio = () => {
     <section className="portfolio">
       <Container>
         <div className="portfolio-cards">
-            <div className="card gray" data-season="gray">
+          <div className={active === 'portfolio' ? 'card gray active-text' : 'card gray'} data-name="gray">
+            <div className="card-text">
               <h2 onClick={handleClick} data-value={repos[0]}>Project name: {repos[0]}</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p>link to Github: {data.git_url}</p>
             </div>
-            <div className="card shadow-gray" data-season="shadow-gray">
+          </div>
+          <div className={active === 'Feedback-UI' ? 'card shadow-gray active-text' : 'card shadow-gray'} data-name="shadow-gray">
+            <div className="card-text">
               <h2 onClick={handleClick} data-value={repos[1]}>Project name: {repos[1]}</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
-            <div className="card silver" data-season="silver">
+          </div>
+          <div className={active === 'Github-Finder' ? 'card silver active-text' : 'card silver'} data-name="silver">
+            <div className="card-text">
               <h2 onClick={handleClick} data-value={repos[2]}>Project name: {repos[2]}</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
-            <div className="card silver-black" data-season="silver-black">
+          </div>
+          <div className={active === 'ReactWeather-app' ? 'card silver-black active-text' : 'card silver-black'} data-name="silver-black">
+            <div className="card-text">
               <h2 onClick={handleClick} data-value={repos[3]}>Project name: {repos[3]}</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
-            <div className="card gray-black" data-season="gray-black">
+          </div>
+          <div className={active === 'Track_calories' ? 'card gray-black active-text' : 'card gray-black'} data-name="gray-black">
+            <div className="card-text">
               <h2 onClick={handleClick} data-value={repos[4]}>Project name: {repos[4]}</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
-            <div className="card black" data-season="black">
+          </div>
+          <div className={active === 'calculate-car-excise' ? 'card black active-text' : 'card black'} data-name="black">
+            <div className="card-text">
               <h2 onClick={handleClick} data-value={repos[5]}>Project name: {repos[5]}</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
+          </div>
         </div>
       </Container>
     </section>
