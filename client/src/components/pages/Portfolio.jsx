262f3a3b1f4e1223441868c3f2a13a4819f2746c
Malzagic <przemek2142@gmail.com>
2023-06-21 15:28:28 +0200

    huge update of portfolio component with using fetch data and add some assets to better display of content

--- client/src/components/pages/Portfolio.jsx
+++ client/src/components/pages/Portfolio.jsx
@@ -4,16 +4,24 @@ import {
 } from "react-router-dom";
 import axios from 'axios';
 import Container from "../../shared/components/UX/containers/Container";
-import { Oval} from 'react-loader-spinner';
+import { Oval } from 'react-loader-spinner';
+import portfolio from '../../assets/github-projects/portfolio.png';
+import feedbackUI from '../../assets/github-projects/Feedback-UI.png';
+import githubFinder from '../../assets/github-projects/Github-Finder.png';
+import reactWeatherApp from '../../assets/github-projects/ReactWeather-app.png';
+import trackCalories from '../../assets/github-projects/Track_calories.png';
+import calculateCarExcise from '../../assets/github-projects/calculate-car-excise.png';
 
 import './Portfolio.css';
 
 const Portfolio = () => {
   const repos = ['portfolio', 'Feedback-UI', 'Github-Finder', 'ReactWeather-app', 'Track_calories', 'calculate-car-excise'];
+  const reposImg = [portfolio, feedbackUI, githubFinder, reactWeatherApp, trackCalories, calculateCarExcise];
 
   const [repositories, setRepositories] = useState(repos[0]);
   const [active, setActive] = useState(repos[0]);
   const [data, setData] = useState({});
+  const [userData, setUserData] = useState({});
   const [loading, setLoading] = useState(false);
 
   const fetchData = async (repo) => {
@@ -22,8 +30,9 @@ const Portfolio = () => {
       const response = await axios.get(url + repo);
       const jsonData = await response.data;
 
-      setLoading(false);
       setData(jsonData[0])
+      setUserData(jsonData[0].owner);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -42,13 +51,13 @@ const Portfolio = () => {
     setLoading(true)
   }
 
-  console.log(data)
+  console.log(userData)
   return (
     <section className="portfolio">
       <Container>
         <div className="portfolio-cards">
-          {repos.map(items => (
-            <div key={items.length} className="card">
+          {repos.map((items, index) => (
+            <div key={index} className="card">
               <h2 onClick={handleClick} data-value={items}>Project name: <span data-value={items}>{items.toUpperCase()}</span></h2>
               <div className={`card-item ${active !== items ? '' : 'active-card'}`}>
                 <Oval
@@ -62,11 +71,19 @@ const Portfolio = () => {
                   strokeWidthSecondary={3}
                 />
                 {!loading &&
-                  <div className="">
-                    <h3>test</h3>
-                    <Link to={data.html_url}>Github</Link>
-                    <Link to={data.homepage}>Live</Link>
-                    <p>{data.language}</p>
+                  <div className="item-box">
+                    <div className="item-img">
+                      <img src={reposImg[index]} alt="" />
+                    </div>
+                    <div className="item-text">
+                      <h3>Author: {userData.login}</h3>
+                      <div className="item-links">
+                        <Link className="portfolio-link portfolio-btn" to={userData.html_url}>Github</Link>
+                        <Link className="portfolio-link portfolio-btn" to={data.html_url}>Code</Link>
+                        <Link className="portfolio-link portfolio-btn" to={data.homepage}>Live</Link>
+                      </div>
+                      <p>Languages used: {data.language}</p>
+                    </div>
                   </div>
                 }
               </div>
