5e85fb6cc4eceffdc5a9f447951191f5b202135c
Malzagic <przemek2142@gmail.com>
2023-06-24 15:55:44 +0200

    add new styling to input and textarea fields

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -4,12 +4,16 @@ import SectionTitle from "../../shared/components/UX/sectionTitles/SectionTitle"
 
 import './Contact.css';
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+
 const Contact = () => {
   return (
     <section>
       <Container>
         <SectionTitle title='Contact' />
-        <form action="" className="contact-form">
+        <form action="" className="contact-form" data-aos="fade-right">
           <label htmlFor="name">Name</label>
           <input type="text" id="name"/>
           <label htmlFor="surname">Surname</label>
