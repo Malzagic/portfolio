7bf02bdf75dea825c17d0fb86f3fdb57bee8823b
Malzagic <przemek2142@gmail.com>
2023-06-25 13:51:39 +0200

    change styles for form component and create f handler

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from '../../shared/components/UX/containers/Container';
 import SectionTitle from "../../shared/components/UX/sectionTitles/SectionTitle";
 
@@ -9,20 +9,41 @@ import 'aos/dist/aos.css';
 AOS.init();
 
 const Contact = () => {
+
+  const [nameValue, setNameValue] = useState('');
+  const [surnameValue, setSurnameValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [messangeValue, setMessangeValue] = useState('');
+
+  const sendEmailHandler = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      name: nameValue,
+      surname: surnameValue,
+      email: emailValue,
+      messange: messangeValue,
+    }
+
+    console.log(formData);
+
+  }
+
+
   return (
     <section>
       <Container>
         <SectionTitle title='Contact' />
-        <form action="" className="contact-form" data-aos="fade-right">
+        <form onSubmit={sendEmailHandler} className="contact-form" data-aos="fade-right">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name"/>
+          <input onChange={(e) => setNameValue(e.target.value)} type="text" id="name"/>
           <label htmlFor="surname">Surname</label>
-          <input type="text" id="surname"/>
+          <input onChange={(e) => setSurnameValue(e.target.value)} type="text" id="surname"/>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email"/>
+          <input onChange={(e) => setEmailValue(e.target.value)}  type="email" id="email"/>
           <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" cols="50" rows="10" />
-          <input type="submit" value={'submit'}/>
+          <textarea onChange={(e) => setMessangeValue(e.target.value)} name="message" id="message" cols="50" rows="10" required/>
+          <input className={'btn sendBtn'} type="submit" value={'Submit'}/>
         </form>
       </Container>
     </section>
