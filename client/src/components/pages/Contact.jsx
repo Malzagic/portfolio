565d2a081077dc87dd5e7cb1195c16fe32d85526
Malzagic <przemek2142@gmail.com>
2023-06-26 01:15:25 +0200

    add a react toastify component to show alerts system

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -7,6 +7,9 @@ import './Contact.css';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 AOS.init();
 
 const Contact = () => {
@@ -34,9 +37,9 @@ const Contact = () => {
           "Content-Type": "application/json"
         }
       }).then(res => {
-        if(res.status === 200) {
+        if (res.status === 200) {
           const message = res.data;
-          alert(message.message);
+          toast.success('Email send!');
 
           setNameValue('');
           setSurnameValue('');
@@ -46,6 +49,11 @@ const Contact = () => {
       })
     } catch (error) {
       console.error(error)
+      if(error.message === 'Network Error') {
+        toast.warn('Error internet connection!')
+      } else {
+        toast.error('Something went wrong!')
+      }
     }
   }
 
@@ -54,16 +62,28 @@ const Contact = () => {
     <section className="contact">
       <Container>
         <SectionTitle title='Contact' />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
         <form onSubmit={sendEmailHandler} className="contact-form" data-aos="fade-right">
           <label htmlFor="name">Name</label>
-          <input onChange={(e) => setNameValue(e.target.value)} type="text" id="name" value={nameValue} required/>
+          <input onChange={(e) => setNameValue(e.target.value)} type="text" id="name" value={nameValue} required />
           <label htmlFor="surname">Surname</label>
-          <input onChange={(e) => setSurnameValue(e.target.value)} type="text" id="surname" value={surnameValue} required/>
+          <input onChange={(e) => setSurnameValue(e.target.value)} type="text" id="surname" value={surnameValue} required />
           <label htmlFor="email">Email</label>
-          <input onChange={(e) => setEmailValue(e.target.value)}  type="email" id="email" value={emailValue} required/>
+          <input onChange={(e) => setEmailValue(e.target.value)} type="email" id="email" value={emailValue} required />
           <label htmlFor="message">Message</label>
-          <textarea onChange={(e) => setMessageValue(e.target.value)} name="message" id="message" cols="50" rows="10" value={messageValue} required/>
-          <input className={'btn sendBtn'} type="submit" value={'Submit'}/>
+          <textarea onChange={(e) => setMessageValue(e.target.value)} name="message" id="message" cols="50" rows="10" value={messageValue} required />
+          <input className={'btn sendBtn'} type="submit" value={'Submit'} />
         </form>
       </Container>
     </section>
