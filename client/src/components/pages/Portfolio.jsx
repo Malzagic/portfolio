0ffddd83b48592d7b1d9bfe4a61bf4d87c3a4631
Malzagic <przemek2142@gmail.com>
2023-06-28 00:44:56 +0200

    end of work for deploy my server with correct ssl certificate and itergate it with frontend

--- client/src/components/pages/Portfolio.jsx
+++ client/src/components/pages/Portfolio.jsx
@@ -33,7 +33,7 @@ const Portfolio = () => {
 
   const fetchData = async (repo) => {
     try {
-      const url = `http://vps-69fb7bf5.vps.ovh.net:8000/portfolio/`;
+      const url = `https://www.pmdev.ovh/portfolio/`;
       console.log(url + repo)
       const response = await axios.get(url + repo);
       const jsonData = await response.data;
