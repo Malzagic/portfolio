5128ff50a4ff5477e63c651d26b0bfa40c647093
Malzagic <przemek2142@gmail.com>
2023-08-26 06:33:12 +0200

    update resume section

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -38,7 +38,6 @@ const Portfolio = () => {
       setRepositories([...data])
       setLoading(false);
     } catch (error) {
-      console.error(error);
       if (error.message === 'Network Error') {
         toast.warn('Error server connection!')
       } else {
@@ -82,62 +81,60 @@ const Portfolio = () => {
         />
         {/* Here we have cards with portfolio projects */}
         <div className="portfolio-cards" data-aos="fade-right">
-            <Oval
-              color="#fff"
-              wrapperStyle={{}}
-              wrapperClass="card-loader"
-              visible={loading}
-              ariaLabel='oval-loading'
-              secondaryColor="#000"
-              strokeWidth={3}
-              strokeWidthSecondary={3}
-            />
-             {repositories.map((item, index) => (
-              <div key={index} className="card" onClick={e => handleClick(e)}>
-                <h2
-                  data-value={item.name}
-                >
-                  {
-                    item.name.replace(/[^a-zA-Z0-9 ]/g, ' ').toUpperCase()
-                  }
-                </h2>
-                <div className={`card-item ${isActive === item.name ? "active-card" : ""}`}>
-                  <div className="item-box">
-                    <div className="item-img">
-                      <img src={reposImg[index]} alt={item.name} />
+          {/* loader */}
+          <Oval
+            color="#fff"
+            wrapperStyle={{}}
+            wrapperClass="card-loader"
+            visible={loading}
+            ariaLabel='oval-loading'
+            secondaryColor="#000"
+            strokeWidth={3}
+            strokeWidthSecondary={3}
+          />
+          {/* map through all repositories */}
+          {repositories.map((item, index) => (
+            <div key={index} className="card" onClick={e => handleClick(e)}>
+              <h2 data-value={item.name} >
+                {item.name.replace(/[^a-zA-Z0-9 ]/g, ' ').toUpperCase()}
+              </h2>
+              <div className={`card-item ${isActive === item.name ? "active-card" : ""}`}>
+                <div className="item-box">
+                  <div className="item-img">
+                    <img src={reposImg[index]} alt={item.name} />
+                  </div>
+                  <div className="item-text">
+                    <h3>Author: {item.owner.login}</h3>
+                    <div className="item-links">
+                      <LinkButton
+                        className="portfolio-btn"
+                        link={item.html_url}
+                        target="_blank"
+                        text={'Github'}
+                      />
+                      <LinkButton
+                        className="portfolio-btn"
+                        link={item.html_url}
+                        target="_blank"
+                        text={'Code'}
+                      />
+                      <LinkButton
+                        className="portfolio-btn"
+                        link={item.homepage}
+                        target="_blank"
+                        text={'Live'}
+                      />
                     </div>
-                    <div className="item-text">
-                      <h3>Author: {item.owner.login}</h3>
-                      <div className="item-links">
-                        <LinkButton
-                          className="portfolio-btn"
-                          link={item.html_url}
-                          target="_blank"
-                          text={'Github'}
-                        />
-                        <LinkButton
-                          className="portfolio-btn"
-                          link={item.html_url}
-                          target="_blank"
-                          text={'Code'}
-                        />
-                        <LinkButton
-                          className="portfolio-btn"
-                          link={item.homepage}
-                          target="_blank"
-                          text={'Live'}
-                        />
-                      </div>
-                      <p>Languages used: {item.language}</p>
-                      <div className="item-description">
-                        <h4>Description:</h4>
-                        <p>{item.description}</p>
-                      </div>
+                    <p>Languages used: {item.language}</p>
+                    <div className="item-description">
+                      <h4>Description:</h4>
+                      <p>{item.description}</p>
                     </div>
                   </div>
                 </div>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
       </Container>
     </section>
--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -22,7 +22,7 @@ const Resume = () => {
               <h6 className="resume-summary-title">2018 - Presents</h6>
             </div>
             <div className="resume-details">
-              <h5 className="resume-details-title">IT Support & Services</h5>
+              <h5 className="resume-details-title">IT Support Technician</h5>
               <h6 className="resume-details-link"><Link to={`https://www.facebook.com/mpowroznikcom`} target="_blank">mpowroznik.com</Link></h6>
               <p>IT services and support for cooperating companies. Creating websites from scratch and by using CMS ( Wordpress, Elementar ).</p>
             </div>
@@ -58,7 +58,7 @@ const Resume = () => {
             </div>
             <div className="resume-details">
               <h5 className="resume-details-title">Modern HTML & CSS from beginning(including SASS)</h5>
-              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/`} target="_blank">Udemy Course</Link></h6>
+              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/course/modern-html-css-from-the-beginning/`} target="_blank">Udemy Course</Link></h6>
               <p>
                 In 17 October 2017 - Complete full course from udemy platform in english languages by Brad Traversy.
               </p>
@@ -70,7 +70,7 @@ const Resume = () => {
             </div>
             <div className="resume-details">
               <h5 className="resume-details-title">JavaScript</h5>
-              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/`} target="_blank">Udemy Course</Link></h6>
+              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/course/javascript-jedyny-kurs-ktorego-potrzebujesz/`} target="_blank">Udemy Course</Link></h6>
               <p>
                 In 4 April 2022 - Complete full course from udemy platform in polish languages by Mateusz Maj.
               </p>
@@ -82,7 +82,7 @@ const Resume = () => {
             </div>
             <div className="resume-details">
               <h5 className="resume-details-title">JavaScript - 10 project's in clear JS</h5>
-              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/`} target="_blank">Udemy Course</Link></h6>
+              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/course/10-projektow-w-czystym-javascript-cz-1/`} target="_blank">Udemy Course</Link></h6>
               <p>
                 In 14 June 2022 - Complete full course from udemy platform in polish languages by Mateusz Maj.
               </p>
@@ -94,7 +94,7 @@ const Resume = () => {
             </div>
             <div className="resume-details">
               <h5 className="resume-details-title">15 components for websites</h5>
-              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/`} target="_blank">Udemy Course</Link></h6>
+              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/course/komponenty-na-strony-www/`} target="_blank">Udemy Course</Link></h6>
               <p>
                 In 22 June 2022 - Complete full course from udemy platform in polish languages by Mateusz Maj. It's a course where I practice how to use HTML, CSS & JS to combine into a tricky components for websites.
               </p>
@@ -106,7 +106,7 @@ const Resume = () => {
             </div>
             <div className="resume-details">
               <h5 className="resume-details-title">Vanilla JavaScript - 15 project's with OOP</h5>
-              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/`} target="_blank">Udemy Course</Link></h6>
+              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/course/vanilla-js-stworz-projekty-w-czystym-javascript-od-podstaw/`} target="_blank">Udemy Course</Link></h6>
               <p>
                 In 5 July 2022 - Complete full course from udemy platform in polish languages by Kuba Wąsikowski.
               </p>
@@ -118,7 +118,7 @@ const Resume = () => {
             </div>
             <div className="resume-details">
               <h5 className="resume-details-title">Web development - from zero to master</h5>
-              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/`} target="_blank">Udemy Course</Link></h6>
+              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/course/web-developer-bootcamp-tworzenie-stron-www-od-zera-do-mastera/`} target="_blank">Udemy Course</Link></h6>
               <p>
                 In 31 July 2022 - Complete full course from udemy platform in polish languages by Kuba Wąsikowski.
               </p>
@@ -130,7 +130,7 @@ const Resume = () => {
             </div>
             <div className="resume-details">
               <h5 className="resume-details-title">Modern JavaScript from the beginning</h5>
-              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/`} target="_blank">Udemy Course</Link></h6>
+              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/course/modern-javascript-from-the-beginning/`} target="_blank">Udemy Course</Link></h6>
               <p>
                 In 31 July 2022 - Complete full course from udemy platform in english languages by Brad Traversy.
               </p>
@@ -142,7 +142,7 @@ const Resume = () => {
             </div>
             <div className="resume-details">
               <h5 className="resume-details-title">React, NodeJS, Express & MongoDB - MERN Stack</h5>
-              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/`} target="_blank">Udemy Course</Link></h6>
+              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/course/react-nodejs-express-mongodb-the-mern-fullstack-guide/`} target="_blank">Udemy Course</Link></h6>
               <p>
                 In 2023 - I started and continue course from Maximilian Schwarzmuller about being full/mern stack developer.
               </p>
@@ -153,10 +153,10 @@ const Resume = () => {
               <h6 className="resume-summary-title">2023 - Presents</h6>
             </div>
             <div className="resume-details">
-              <h5 className="resume-details-title">Flutter & Dart to complete guid</h5>
-              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/`} target="_blank">Udemy Course</Link></h6>
+              <h5 className="resume-details-title">Understanding TypeScript</h5>
+              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/course/understanding-typescript/`} target="_blank">Udemy Course</Link></h6>
               <p>
-                In 2023 - I started and continue course from Maximilian Schwarzmuller about flutter framework to create functionality mobile application for both platforms IOS/Android.
+                In 2023 - I started and continue course from Maximilian Schwarzmuller about typescript languages to understand more for projects in React.
               </p>
             </div>
           </div>
@@ -165,10 +165,10 @@ const Resume = () => {
               <h6 className="resume-summary-title">2023 - Presents</h6>
             </div>
             <div className="resume-details">
-              <h5 className="resume-details-title">Understanding TypeScript</h5>
-              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/`} target="_blank">Udemy Course</Link></h6>
+              <h5 className="resume-details-title">The Complete 2023 Web Development Bootcamp</h5>
+              <h6 className="resume-details-link"><Link to={`https://www.udemy.com/course/the-complete-web-development-bootcamp/`} target="_blank">Udemy Course</Link></h6>
               <p>
-                In 2023 - I started and continue course from Maximilian Schwarzmuller about typescript languages to understand more for projects in React.
+                In 2023 - I started and continue course from DR. Angela Yu about complete knownlage for web development from frontend to backend.
               </p>
             </div>
           </div>
