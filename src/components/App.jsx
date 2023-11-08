645db4a8113d329e610d9c43eedfdf231d8e8653
Malzagic <przemek2142@gmail.com>
2023-11-08 16:22:38 +0100

    add suspense and change styling for loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Wrapper from '../shared/components/UX/containers/Wrapper';
-import Navigation from '../shared/components/navigation/Navigation';
-import Home from './pages/Home';
-import About from './pages/About';
-import Resume from './pages/Resume';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
+import Wrapper from "../shared/components/UX/containers/Wrapper";
+import Navigation from "../shared/components/navigation/Navigation";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Resume from "./pages/Resume";
+// import Portfolio from "./pages/Portfolio";
+// import Contact from "./pages/Contact";
+import { Oval } from "react-loader-spinner";
 
+const Home = React.lazy(() => import("./pages/Home"));
+const About = React.lazy(() => import("./pages/About"));
+const Resume = React.lazy(() => import("./pages/Resume"));
+const Portfolio = React.lazy(() => import("./pages/Portfolio"));
+const Contact = React.lazy(() => import("./pages/Contact"));
 
 const App = () => {
   return (
@@ -16,17 +22,34 @@ const App = () => {
       <BrowserRouter>
         <Wrapper>
           <Navigation />
-          <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/about' element={<About />}/>
-            <Route path='/resume' element={<Resume />}/>
-            <Route path='/portfolio' element={<Portfolio />}/>
-            <Route path='/contact' element={<Contact />}/>
-          </Routes>
+          <Suspense
+            fallback={
+              <div className="center">
+                <Oval
+                  color="#fff"
+                  wrapperStyle={{}}
+                  wrapperClass="card-loader"
+                  visible={true}
+                  ariaLabel="oval-loading"
+                  secondaryColor="#000"
+                  strokeWidth={3}
+                  strokeWidthSecondary={3}
+                />
+              </div>
+            }
+          >
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/resume" element={<Resume />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </Suspense>
         </Wrapper>
       </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,32 +1,23 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import Container from "../../shared/components/UX/containers/Container";
 import SectionTitle from "../../shared/components/UX/sectionTitles/SectionTitle";
-import LinkButton from '../../shared/components/UX/buttons/LinkButton';
-import { Oval } from 'react-loader-spinner';
-import portfolio from '../../assets/github-projects/portfolio.png';
-import feedbackUI from '../../assets/github-projects/Feedback-UI.png';
-import githubFinder from '../../assets/github-projects/Github-Finder.png';
-import reactWeatherApp from '../../assets/github-projects/ReactWeather-app.png';
-import trackCalories from '../../assets/github-projects/Track_calories.png';
-import NewslatterSignup from '../../assets/github-projects/Newslatter_Signup.png'
+import LinkButton from "../../shared/components/UX/buttons/LinkButton";
+import { Oval } from "react-loader-spinner";
 
-import './Portfolio.css';
+import "./Portfolio.css";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 AOS.init();
 
 const Portfolio = () => {
-  const reposImg = [portfolio, feedbackUI, githubFinder, reactWeatherApp, trackCalories, NewslatterSignup];
-
   const [repositories, setRepositories] = useState([]);
   const [isActive, setIsActive] = useState(false);
   const [loading, setLoading] = useState(false);
-
 
   // FETCHING DATA FROM SERVER
   const fetchData = async () => {
@@ -34,14 +25,13 @@ const Portfolio = () => {
       const url = `https://www.pmdev.ovh/api/portfolio`;
       const response = await axios.get(url);
       const data = response.data;
-
-      setRepositories([...data])
+      setRepositories([...data]);
       setLoading(false);
     } catch (error) {
-      if (error.message === 'Network Error') {
-        toast.warn('Error server connection!')
+      if (error.message === "Network Error") {
+        toast.warn("Error server connection!");
       } else {
-        toast.error('Something went wrong!')
+        toast.error("Something went wrong!");
       }
     }
   };
@@ -54,19 +44,31 @@ const Portfolio = () => {
     }
 
     setIsActive(repo);
-  }
-
+  };
 
   // WHEN IS DINAMICLY CHANGED
   useEffect(() => {
-    fetchData()
+    fetchData();
     setLoading(true);
-  }, [])
+  }, []);
 
   return (
     <section className="portfolio">
       <Container>
-        <SectionTitle title='Portfolio' />
+        <SectionTitle title="Portfolio" />
+        {/* loader */}
+        <div className="center">
+            <Oval
+              color="#fff"
+              wrapperStyle={{}}
+              wrapperClass="card-loader"
+              visible={loading}
+              ariaLabel="oval-loading"
+              secondaryColor="#000"
+              strokeWidth={3}
+              strokeWidthSecondary={3}
+            />
+          </div>
         <ToastContainer
           position="top-right"
           autoClose={5000}
@@ -81,24 +83,17 @@ const Portfolio = () => {
         />
         {/* Here we have cards with portfolio projects */}
         <div className="portfolio-cards" data-aos="fade-right">
-          {/* loader */}
-          <Oval
-            color="#fff"
-            wrapperStyle={{}}
-            wrapperClass="card-loader"
-            visible={loading}
-            ariaLabel='oval-loading'
-            secondaryColor="#000"
-            strokeWidth={3}
-            strokeWidthSecondary={3}
-          />
           {/* map through all repositories */}
           {repositories.map((item, index) => (
-            <div key={index} className="card" onClick={e => handleClick(e)}>
-              <h2 data-value={item.name} >
-                {item.name.replace(/[^a-zA-Z0-9 ]/g, ' ').toUpperCase()}
+            <div key={index} className="card" onClick={(e) => handleClick(e)}>
+              <h2 data-value={item.name}>
+                {item.name.replace(/[^a-zA-Z0-9 ]/g, " ").toUpperCase()}
               </h2>
-              <div className={`card-item ${isActive === item.name ? "active-card" : ""}`}>
+              <div
+                className={`card-item ${
+                  isActive === item.name ? "active-card" : ""
+                }`}
+              >
                 <div className="item-box">
                   <div className="item-img">
                     {/* <img src={reposImg[index]} alt={item.name} /> */}
@@ -110,19 +105,19 @@ const Portfolio = () => {
                         className="portfolio-btn"
                         link={item.html_url}
                         target="_blank"
-                        text={'Github'}
+                        text={"Github"}
                       />
                       <LinkButton
                         className="portfolio-btn"
                         link={item.html_url}
                         target="_blank"
-                        text={'Code'}
+                        text={"Code"}
                       />
                       <LinkButton
                         className="portfolio-btn"
                         link={item.homepage}
                         target="_blank"
-                        text={'Live'}
+                        text={"Live"}
                       />
                     </div>
                     <p>Languages used: {item.language}</p>
@@ -138,7 +133,7 @@ const Portfolio = () => {
         </div>
       </Container>
     </section>
-  )
-}
+  );
+};
 
 export default Portfolio;
