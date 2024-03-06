df46007369e0b43fb0cffb89b3cc65921ff38ce9
Malzagic <przemek2142@gmail.com>
2024-03-06 20:32:53 +0100

    rebuild skills section

--- src/components/about/SkillsGrid.jsx
+++ src/components/about/SkillsGrid.jsx
@@ -2,16 +2,81 @@ import React from "react";
 import Grid from "../../shared/components/UI/containers/Grid";
 import Col from "../../shared/components/UI/containers/Col";
 
-import { FaReact, FaHtml5 } from "react-icons/fa";
+import {
+  FaReact,
+  FaHtml5,
+  FaCss3Alt,
+  FaNode,
+  FaBootstrap,
+  FaPython,
+  FaLinux,
+} from "react-icons/fa";
+import {
+  SiJavascript,
+  SiNextdotjs,
+  SiJquery,
+  SiTypescript,
+  SiMongodb,
+  SiSqlite,
+  SiTailwindcss,
+  SiFirebase,
+  SiNginx,
+} from "react-icons/si";
+import { VscJson } from "react-icons/vsc";
 
 export default function SkillsGrid() {
   return (
     <Grid aos="fade-right">
-      <Col title={"React"}>
+      <Col title={"HTML"}>
+        <FaHtml5 className="mt2 fs-3" />
+      </Col>
+      <Col title={"CSS"}>
+        <FaCss3Alt className="mt2 fs-3" />
+      </Col>
+      <Col title={"JavaScript"}>
+        <SiJavascript className="mt2 fs-3" />
+      </Col>
+      <Col title={"ReactJS"}>
         <FaReact className="mt2 fs-3" />
       </Col>
-      <Col>
-        <FaHtml5 className="mt2 fs-3"/>
+      <Col title={"NextJS"}>
+        <SiNextdotjs className="mt2 fs-3" />
+      </Col>
+      <Col title={"NodeJS"}>
+        <FaNode className="mt2 fs-3" />
+      </Col>
+      <Col title={"REST API"}>
+        <VscJson className="mt2 fs-3" />
+      </Col>
+      <Col title={"JQuery"}>
+        <SiJquery className="mt2 fs-3" />
+      </Col>
+      <Col title={"TypeScript"}>
+        <SiTypescript className="mt2 fs-3" />
+      </Col>
+      <Col title={"MongoDB"}>
+        <SiMongodb className="mt2 fs-3" />
+      </Col>
+      <Col title={"SQLite"}>
+        <SiSqlite className="mt2 fs-3" />
+      </Col>
+      <Col title={"Bootstrap"}>
+        <FaBootstrap className="mt2 fs-3" />
+      </Col>
+      <Col title={"TailwindCSS"}>
+        <SiTailwindcss className="mt2 fs-3" />
+      </Col>
+      <Col title={"Firebase"}>
+        <SiFirebase className="mt2 fs-3" />
+      </Col>
+      <Col title={"Python"}>
+        <FaPython className="mt2 fs-3" />
+      </Col>
+      <Col title={"Linux"}>
+        <FaLinux className="mt2 fs-3" />
+      </Col>
+      <Col title={"Nginx"}>
+        <SiNginx className="mt2 fs-3" />
       </Col>
       {/* <Col title={"HTML"}>
         <FontAwesomeIcon icon={faHtml5} className="mt2 fs-3" />
